ab50e1478c5bd92fc6074b9f30b673b62d276bde
Marcos Leite <m.abner@live.co.uk>
2024-01-02 23:19:03 -0300

    NaoDeveConterAPalavraTesteNoTexto: fixes assert parameter

--- TestesUnitarios.Desafio.Tests/ValidacoesStringTests.cs
+++ TestesUnitarios.Desafio.Tests/ValidacoesStringTests.cs
@@ -45,14 +45,14 @@ public class ValidacoesStringTests
         var resultado = _validacoes.ContemCaractere(texto, textoProcurado);
 
         // Assert
-        //TODO: Corrigir o Assert.False com base no retorno da chamada ao método
-        Assert.False(true);
+        Assert.False(resultado);
     }
 
     //TODO: Corrigir a anotação [Fact]
     public void TextoDeveTerminarComAPalavraProcurado()
     {
         //TODO: Corrigir a variável "textoProcurado" seção Arrange
+
 
         // Arrange
         var texto = "Começo, meio e fim do texto procurado";
